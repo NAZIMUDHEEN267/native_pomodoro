c1e3a8c31386b132aec09d475425178a5d855079
nazimudheen267 <nazimudheen267@gmail.com>
2022-11-05 22:10:52 +0530

    iphone style added

---
+++ Components/Home/Home.ios.js
@@ -0,0 +1,29 @@
+import { View, Text } from 'react-native';
+import styles from "./Home.styles";
+import Icon from "react-native-vector-icons/MaterialIcons";
+import Timer from '../Timer/Timer';
+import { Button } from '../Button';
+// import {  } from "react-native-reanimated";
+
+const Home = () => {
+
+    const colors = ["#dd8b76", "#76ddce", "#ab87e7"];
+    const buttons = ["pomodoro", "short break", "long break"];
+
+    return (
+        <View style={styles.container}>
+            <Text style={styles.headline}>pomodoro</Text>
+            <View style={styles.nav}>
+                {
+                    buttons.map((text, i) => {
+                        return <Button btnStyle={styles.btnStyle} text={text} clicked={i == 0 ? colors[2] : 'transparent'} key={text + i} />
+                    })
+                }
+            </View>
+            <Timer color={colors[2]} />
+            <Icon name='settings' size={40} color={"darkgray"} onPress={() => alert("hello user")}></Icon>
+        </View>
+    )
+}
+
+export default Home;
